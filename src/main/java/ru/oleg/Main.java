8394c287876ae685f99f7f7d9132a95a09ab8d9c
PowerOleg <geraskin.oleg@mail.ru>
2023-05-03 23:18:36 +0500

    did launch from console;2.8-snapshot

--- src/main/java/ru/oleg/Main.java
+++ src/main/java/ru/oleg/Main.java
@@ -5,17 +5,11 @@ import ru.oleg.services.SortService;
 import ru.oleg.services.SortServiceImpl;
 
 import java.io.File;
-import java.net.URL;
-import java.util.Objects;
-
 public class Main {
     public static final String FILE_PATH = "example.txt";
 
     public static void main(String[] args) {
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        final URL resource = classLoader.getResource(FILE_PATH);
-        final File inputFile = new File(Objects.requireNonNull(resource).getFile());
-
+        final File inputFile = new File(FILE_PATH);
         SortService service = new SortServiceImpl();
         Controller controller = new Controller(service);
         controller.printSortedItemsConsole(inputFile);
