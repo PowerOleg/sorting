f23e043532217b834e434d49927bc8c22497057f
PowerOleg <geraskin.oleg@mail.ru>
2023-05-03 13:59:20 +0500

    executable jar;trouble to find txt in jar;2.3-snapshot

--- src/main/java/ru/oleg/Main.java
+++ src/main/java/ru/oleg/Main.java
@@ -5,13 +5,37 @@ import ru.oleg.services.SortService;
 import ru.oleg.services.SortServiceImpl;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Objects;
 
 public class Main {
-    public static final String FILE_PATH = "src/main/java/ru/oleg/examples/example.txt";
+    public static final String BASE_PACKAGE = "ru.oleg.examples";
     public static void main(String[] args) {
-        File inputFile = new File(FILE_PATH);
-        SortService service = new SortServiceImpl();
-        Controller controller = new Controller(service);
-        controller.printSortedItemsConsole(inputFile);
+        File inputFile = null;
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        try {
+            Enumeration<URL> resources = classLoader.getResources(BASE_PACKAGE.replace('.', '/'));
+            while (resources.hasMoreElements()) {
+                File dir = new File(resources.nextElement().toURI());
+//                for (File f : Objects.requireNonNull(dir.listFiles())) {
+                    inputFile = dir;
+////                    if (f.getName().endsWith("txt")) {
+////                        String fileName = f.getName().substring(0, f.getName().lastIndexOf("."));
+////                    }
+//                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+            SortService service = new SortServiceImpl();
+            Controller controller = new Controller(service);
+            controller.printSortedItemsConsole(inputFile);
+        }
     }
-}
