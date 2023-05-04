4e3f5ff9ef59b4dd449796be5bb9f0b10b431374
PowerOleg <geraskin.oleg@mail.ru>
2023-05-04 15:29:38 +0500

    SeTest,DetTest are done;3.7-snapshot

--- src/test/java/ru/oleg/models/DetTest.java
+++ src/test/java/ru/oleg/models/DetTest.java
@@ -1,4 +1,23 @@
 package ru.oleg.models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DetTest {
+
+    @Test
+    public void test_det_constructor_and_getters_positive() {
+        final int expectedType = 2;
+        final int expectedPosition = 100;
+        final int expectedSystem = 20;
+        final int expectedNumber = 1;
+        final String expectedFullname = "DET 100-20-0001";
+        final Det det = new Det(expectedPosition, expectedSystem, expectedNumber, expectedFullname);
+
+        Assertions.assertEquals(expectedPosition, det.getPositionNumber());
+        Assertions.assertEquals(expectedSystem, det.getSystemNumber());
+        Assertions.assertEquals(expectedNumber, det.getNumber());
+        Assertions.assertEquals(expectedType, det.getType());
+        Assertions.assertEquals(expectedFullname, det.getFullname());
+    }
 }
--- src/test/java/ru/oleg/models/SeTest.java
+++ src/test/java/ru/oleg/models/SeTest.java
@@ -1,4 +1,23 @@
 package ru.oleg.models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SeTest {
+
+    @Test
+    public void test_se_constructor_and_getters_positive() {
+        final int expectedType = 1;
+        final int expectedPosition = 100;
+        final int expectedSystem = 20;
+        final int expectedNumber = 700;
+        final String expectedFullname = "SE 100-20-700";
+        final Se se = new Se(expectedPosition, expectedSystem, expectedNumber, expectedFullname);
+
+        Assertions.assertEquals(expectedPosition, se.getPositionNumber());
+        Assertions.assertEquals(expectedSystem, se.getSystemNumber());
+        Assertions.assertEquals(expectedNumber, se.getNumber());
+        Assertions.assertEquals(expectedType, se.getType());
+        Assertions.assertEquals(expectedFullname, se.getFullname());
+    }
 }
