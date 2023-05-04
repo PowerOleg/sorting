41329c452c5b537f9c4c6a118c386ba5bc9efd2c
PowerOleg <geraskin.oleg@mail.ru>
2023-05-04 15:50:45 +0500

    Mat tested;3.9-snapshot

--- src/test/java/ru/oleg/models/MatTest.java
+++ src/test/java/ru/oleg/models/MatTest.java
@@ -4,19 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MatTest {
+
     @Test
     public void test_se_constructor_and_getters_positive() {
-        final int expectedType = 1;
-        final int expectedPosition = 100;
-        final int expectedSystem = 20;
-        final int expectedNumber = 700;
-        final String expectedFullname = "SE 100-20-700";
-        final Se se = new Se(expectedPosition, expectedSystem, expectedNumber, expectedFullname);
+        final int expectedType = 4;
+        final String expectedFullname = "MAT Ацетон ГОСТ 18675";
+        final String expectedName = "Ацетон ГОСТ 18675";
+        final Mat mat = new Mat(expectedFullname, expectedName);
 
-        Assertions.assertEquals(expectedPosition, se.getPositionNumber());
-        Assertions.assertEquals(expectedSystem, se.getSystemNumber());
-        Assertions.assertEquals(expectedNumber, se.getNumber());
-        Assertions.assertEquals(expectedType, se.getType());
-        Assertions.assertEquals(expectedFullname, se.getFullname());
+        Assertions.assertEquals(expectedFullname, mat.getFullname());
+        Assertions.assertEquals(expectedName, mat.getName());
     }
 }
