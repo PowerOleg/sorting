58bb966e3f2845b7920a107666e61cf3a7945775
PowerOleg <geraskin.oleg@mail.ru>
2023-05-04 14:14:59 +0500

    sort() tested;3.4-snapshot

--- src/test/java/ru/oleg/services/SortServiceImplTest.java
+++ src/test/java/ru/oleg/services/SortServiceImplTest.java
@@ -10,6 +10,7 @@ import ru.oleg.models.*;
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SortServiceImplTest {
     SortServiceImpl service;
@@ -17,6 +18,19 @@ public class SortServiceImplTest {
     @BeforeEach
     public void initialization() {
         service = new SortServiceImpl();
+    }
+
+    @Test
+    public void test_parse_identificator_positive() {
+        final int numberOfParts = 3;
+        final String argument = "248-01-0045";
+        int[] expected = new int[numberOfParts];
+        expected[0] = 248;
+        expected[1] = 1;
+        expected[2] = 45;
+
+        int[] result = service.parseIdentificator(argument);
+        Assertions.assertArrayEquals(expected, result);
     }
 
     @Test
@@ -42,7 +56,7 @@ public class SortServiceImplTest {
     }
 
     @Test
-    public void test_get_data_from_txt_positive() {
+    public void test_get_data_from_positive() {
         final int numberOfElementsInTxt = 2;
         Object[] expected = new Object[numberOfElementsInTxt];
         final Se se = new Se(160, 5, 802, "SE 160-05-802");
@@ -54,4 +68,32 @@ public class SortServiceImplTest {
         final Object[] resultArray = result.toArray();
             Assertions.assertArrayEquals(expected, resultArray);
     }
+    @Test
+    public void test_sort_positive() {
+        final int numberOfElements = 14;
+        Item[] expected = new Item[numberOfElements];
+        expected[0] = new Se(160, 5, 801, "SE 160-05-801");
+        expected[1] = new Se(160, 5, 802, "SE 160-05-802");
+        expected[2] = new Se(204, 40, 55,"SE 204-40-055");
+        expected[3] = new Se(205, 40, 50,"SE 205-40-050");
+        expected[4] = new Det(248,1, 45, "DET 248-01-0045");
+        expected[5] = new Det(248,2, 45, "DET 248-02-0045");
+        expected[6] = new Std("C03", 0, 711, 100, "STD C03-711-100");
+        expected[7] = new Std("C03", 0, 711, 200, "STD C03-711-200");
+        expected[8] = new Std("C03", 1, 711, 50, "STD C03-711-01-050");
+        expected[9] = new Std("C03", 0, 712, 1, "STD C03-712-001");
+        expected[10] = new Std("C04", 0, 710, 100, "STD C04-710-100");
+        expected[11] = new Mat("MAT Ацетон ГОСТ 18675", "Ацетон ГОСТ 18675");
+        expected[12] = new Mat("MAT Ацетон ГОСТ 19675", "Ацетон ГОСТ 19675");
+        expected[13] = new Mat("MAT Рукав", "Рукав");
+
+
+        Set<Item> argument = new HashSet<>();
+        for (int i = numberOfElements-1; i >= 0; i--) {
+            argument.add(expected[i]);
+        }
+        Object[] resultArray = service.sort(argument).toArray();
+        Assertions.assertArrayEquals(expected, resultArray);
+    }
+
 }
